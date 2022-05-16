1d500257fa3c8c9014fdec1e649664d5fda655c2
EC2 Default User <ec2-user@ip-172-31-20-97.ec2.internal>
2022-05-16 09:05:16 +0000

    [Update]マイページ・会員情報編集画面作成？

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -1,2 +1,28 @@
 class Public::CustomersController < ApplicationController
+  def show
+    @customer = Customer.find(params[:id])
+  end
+
+  def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to public_customer_path
+  end
+
+  def is_deleted
+  end
+
+  def withdraw
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:family_name, :first_name, :family_name_kana, :first_name_kana,
+    :zipcode, :address, :telephone_number, :email)
+  end
 end
