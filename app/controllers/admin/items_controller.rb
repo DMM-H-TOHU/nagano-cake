f7743294c9543eb032177a22fbf73235ebb9ccba
Kento Nomura <kentojaga0706@icloud.com>
2022-05-17 03:57:19 +0000

    name email 変更

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -8,4 +8,23 @@ class Admin::ItemsController < ApplicationController
     @items = Item.all
   end
 
+  def show
+    @item = Item.find(params[:id])
+  end
+
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render "new"
+    end
+
+  end
+
+  private
+
+  def item_params
+    params.require(:item).permit(:image, :name, :introduction, :genre_id, :price, :is_status)
+  end
 end
