676b3cec36158849b7064c0d28caf96adae27f43
Kento Nomura <kentojaga0706@icloud.com>
2022-05-17 09:02:53 +0000

    show作成中

--- app/models/item.rb
+++ app/models/item.rb
@@ -3,4 +3,11 @@ class Item < ApplicationRecord
   has_many :cart_items
   belongs_to :genre
   has_many :order_details
+  def get_image
+    if image.attached?
+      image
+    else
+      'logo.png'
+    end
+  end
 end
