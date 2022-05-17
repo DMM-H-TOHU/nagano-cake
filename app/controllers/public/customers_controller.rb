40473ab251002bbd89e32795185b64b1503e7342
ringotelyan <sute.dh9999@gmail.com>
2022-05-17 09:03:27 +0000

    [Update]編集画面調整

--- app/controllers/public/customers_controller.rb
+++ app/controllers/public/customers_controller.rb
@@ -11,19 +11,19 @@ class Public::CustomersController < ApplicationController
   def update
     @customer = Customer.find(params[:id])
     @customer.update(customer_params)
-    redirect_to public_customer_path
+    redirect_to public_customers_my_page_path
   end
 
   def is_deleted
   end
 
   def withdraw
-    @customer = Customer.find(current_customer)
+    @customer = Customer.find(current_customer.id)
     # is_deletedカラムをtrueに変更することにより削除フラグを立てる
     @customer.update(is_deleted: true)
     reset_session
     flash[:notice] = "退会処理を実行いたしました"
-    redirect_to root_path
+    redirect_to new_customer_registration
   end
 
   private
