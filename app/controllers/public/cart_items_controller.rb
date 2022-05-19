706ef4442d70bec304c5fe4a037de1bfa9e1a38a
ringotelyan <sute.dh9999@gmail.com>
2022-05-19 07:44:24 +0000

    [Update]カート機能コントローラ準備

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -1,12 +1,32 @@
 class Public::CartItemsController < ApplicationController
+  before_action :authenticate_customer!
 
   def index
-    @total_price = CartItem.total_price
-    @items = CartItem.items
+    @cart_items = current_customer.cart_items.all
   end
-  
+
   def create
-    if 
+    @cart_item = currtent_customer.cart_item.new(cart_item_params)
+    # もしもともとカート内に「同じ商品」がある場合、「数量を追加」更新・保存する
+    # ex)バナナ２個、バナナ２個、ではなく、バナナ「４個」にしたい
+    if current_customer.cart_item.find_by(item_id: params[:cart_item][:item_id].present?)
+          # 元々カート内にあるもの「item_id」
+          # 今追加した　　　　　　　params[:cart_item][:item_id]
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.quantity += params[:cart_item][:amount].to_i
+      # cart_item.quantityに今追加したparams[:cart_item][:quantity]を加える
+      # .to_iとして数字として扱う
+
+      cart_item.save
+      redirect_to cart_items_path
+
+    elsif @cart_item.save
+          @cart_items = current_customer.cart_items.all
+          render 'index'
+    else
+      render 'index'
+    end
+
   private
   def cart_item_params
     params.require(:cart_item).permit(:item_id, :amount)
--- app/controllers/public/items_controller.rb
+++ app/controllers/public/items_controller.rb
@@ -1,13 +1,20 @@
 class Public::ItemsController < ApplicationController
+  before_action :authenticate_customer!, except: [:index, :show]
 
   def index
     @items = Item.page(params[:page])
     @genres = Genre.all
+    @cart_item = CartItem
   end
 
   def show
     @item = Item.find(params[:id])
     @genres = Genre.all
+  end
+
+  private
+  def item_params
+    params.require(:items).permit(:genre_id, :name, :introduction, :image_id, :price)
   end
 
 end
