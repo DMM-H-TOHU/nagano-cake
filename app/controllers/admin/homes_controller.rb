41671cb8eee42d06ad735585be210dc746222cbd
mizu-hisa <101858095+mizu-hisa@users.noreply.github.com>
2022-05-21 04:14:59 +0000

    一覧ページひとまず

--- app/controllers/admin/homes_controller.rb
+++ app/controllers/admin/homes_controller.rb
@@ -1,13 +1,9 @@
 class Admin::HomesController < ApplicationController
 
   def top
-    orders = Order.all
-    order_details = OrderDetails.all
-    # それぞれの複数インスタンスを1つの配列にする
-    @history = orders | order_details
-    # 作成降順（新しい順）に並び替え
-    @history.soft! {|a, b| b.created_at <=> a.created_at }
-    # 作成昇順の場合は、a.created_at <=> b.created_at }
+    @order = Order.all
+    @order_details = OrderDetail.all
+    @order_sorts = Order.all.order(created_at: :desc)
   end
 
 end
