9c20074ae26a5cf8ab1fdaf520be5d2ad86281a2
ringotelyan <sute.dh9999@gmail.com>
2022-05-21 04:14:15 +0000

    [Update]カート機能同一商品追加実装

--- app/controllers/public/cart_items_controller.rb
+++ app/controllers/public/cart_items_controller.rb
@@ -9,16 +9,16 @@ class Public::CartItemsController < ApplicationController
     @cart_item = current_customer.cart_items.new(cart_item_params)
     # もしもともとカート内に「同じ商品」がある場合、「数量を追加」更新・保存する
     # ex)バナナ２個、バナナ２個、ではなく、バナナ「４個」にしたい
-    if current_customer.cart_items.find_by(item_id: params[:cart_item]).present?
+    if current_customer.cart_items.find_by(item_id: params[:item_id]).present?
           # 元々カート内にあるもの「item_id」
           # 今追加した　　　　　　　params[:cart_item][:item_id]
-      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
-      cart_item.amount += params[:cart_item][:amount].to_i
+      cart_item = current_customer.cart_items.find_by(item_id: params[:item_id])
+      cart_item.amount += params[:amount].to_i
       # cart_item.quantityに今追加したparams[:cart_item][:quantity]を加える
       # .to_iとして数字として扱う
 
       cart_item.save
-      redirect_to cart_items_path
+      redirect_to public_cart_items_path
 
     elsif @cart_item.save
           @cart_items = current_customer.cart_items.all
