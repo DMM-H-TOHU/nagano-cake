7d244a26249428d163ec13568ebe65a2029109b0
Kento Nomura <kentojaga0706@icloud.com>
2022-05-21 04:14:13 +0000

    new途中

--- app/controllers/public/orders_controller.rb
+++ app/controllers/public/orders_controller.rb
@@ -1,2 +1,18 @@
 class Public::OrdersController < ApplicationController
+  def new
+    @order = Order.new
+    @customer = Customer.all
+  end
+
+  def index
+  end
+
+  def show
+  end
+
+  def complete
+  end
+
+  def comfirm
+  end
 end
