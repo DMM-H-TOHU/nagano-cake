3317fff6ac8508b4de1daeeabdcba9e1c11a847a
koske0125 <kosuke.0125.dm@gmail.com>
2022-05-24 07:25:03 +0000

    [Fix]色々修正２

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -1,11 +1,10 @@
 class Admin::OrderDetailsController < ApplicationController
 
   def update
-    @order_detail = OrderDetail.find(params[:id])
+    @order_detail = OrderDetail.find(params[:order_detail][:id])
+    @order = @order_detail.order
     if @order_detail.update(order_detail_params)
-      redirect_to admin_order_path(@order_detail)
-    else
-      render 'orders#show'
+      redirect_to admin_order_path(@order)
     end
   end
 
@@ -13,5 +12,5 @@ class Admin::OrderDetailsController < ApplicationController
   def order_detail_params
     params.require(:order_detail).permit(:making_status)
   end
-  
+
 end
