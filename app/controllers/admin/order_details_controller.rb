d93f9cca881ed82b778f270a0c11ee6f4283cd9f
koske0125 <kosuke.0125.dm@gmail.com>
2022-05-25 08:29:15 +0000

    [Add]オーダーステータスが更新されるように

--- app/controllers/admin/order_details_controller.rb
+++ app/controllers/admin/order_details_controller.rb
@@ -3,9 +3,28 @@ class Admin::OrderDetailsController < ApplicationController
   def update
     @order_detail = OrderDetail.find(params[:order_detail][:id])
     @order = @order_detail.order
-    if @order_detail.update(order_detail_params)
-      redirect_to admin_order_path(@order)
+    @order_detail.update(order_detail_params)
+    @order_details = @order.order_details
+
+    if @order_detail.making_status == "making"
+      @order.order_status = "making"
+      @order.save
     end
+
+    decide = 0
+
+    @order_details.each do |order_detail|
+      if order_detail.making_status != "complete"
+        decide += 1
+      end
+    end
+
+    if decide == 0
+      @order.order_status = "preparing"
+      @order.save
+    end
+
+      redirect_to admin_order_path(@order)
   end
 
   private
